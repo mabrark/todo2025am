0cb2ed678b457148e2c35ee5455fac6c3c81efd3
mabrark <muhammad.khan@triosstudent.com>
2025-06-25 11:34:55 -0400

    final version june 25

--- todo/src/app/app.ts
+++ todo/src/app/app.ts
@@ -44,11 +44,12 @@ export class App {
   }
 
   get items(): readonly TodoItem[] {
-    return this.list.items.filter(item => !item.complete);
+    return this.list.items.filter(item => this.showComplete || !item.complete);
     }
   addItem(newItem: string) {
     if (newItem != "") {
      this.list.addItem(newItem);
       }
     }
+    showComplete: boolean = false;
 }
